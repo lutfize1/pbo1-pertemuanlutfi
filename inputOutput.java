69e63ac39b2d4a1f6dc2e393c7b2c86f40d1722e
lutfize1 <lutfize1@gmail.com>
2020-04-18 12:45:29 +0700

    Update inputOutput.java

--- inputOutput.java
+++ inputOutput.java
@@ -3,6 +3,8 @@ import java.util.scanner;
 public class InputOutput{
  public	static void main (String [] args){
   Scanner scanner = new scanner(System.in);
-
+  string nama;
+  System.out.print("Masukkan nama anda: ");
+  nama = scanner.nextLine();
  }
 }
